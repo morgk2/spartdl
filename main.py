11c303822eaee18a4f562523251c005f3d3ab703
morgk2 <tiktoklwcg@gmail.com>
2025-11-25 15:33:42 +0100

    first preview release

--- main.py
+++ main.py
@@ -713,4 +713,6 @@ async def update_metadata_background(task_id: str, file_paths: List[str]):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    import os
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run(app, host="0.0.0.0", port=port)
